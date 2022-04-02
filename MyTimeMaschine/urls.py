a1cd5b17b239a556b1d234aace9b333f71ec2045
Ben Murphy <rasner84@live.com>
2022-04-02 20:22:00 +0200

    init timerecorder

--- MyTimeMaschine/urls.py
+++ MyTimeMaschine/urls.py
@@ -5,11 +5,11 @@ MyTimeMaschine URL Configuration
 """
 from django.contrib import admin
 from django.urls import path, include
-from polls import urls as pollurl
+from timerecorder import urls as time_app
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(pollurl)),
+    path('', include(time_app)),
 ]
---
+++ timerecorder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TimerecorderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'timerecorder'
